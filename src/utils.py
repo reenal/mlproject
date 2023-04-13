cd3128ef1dc56a08e5b3b679a00039392bd047c6
reenal <reenalboddul@gmail.com>
2023-04-13 14:21:48 +0200

    adding application file

--- src/utils.py
+++ src/utils.py
@@ -10,7 +10,6 @@ from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
-
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
@@ -30,12 +29,13 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
         for i in range(len(list(models))):
             model = list(models.values())[i]
             para=param[list(models.keys())[i]]
-            
+
             gs = GridSearchCV(model,para,cv=3)
             gs.fit(X_train,y_train)
-            
+
             model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
+
             #model.fit(X_train, y_train)  # Train model
 
             y_train_pred = model.predict(X_train)
@@ -49,6 +49,14 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
             report[list(models.keys())[i]] = test_model_score
 
         return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
